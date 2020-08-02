133650c19ccab0bd91111d9cfca7c271e86375e5
yspivak <yuliy.spivak@yahoo.com>
2020-08-02 11:21:35 -0400

    Added main function

--- src/lambda_functions.py
+++ src/lambda_functions.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 
 def lambda_client():
     aws_lambda = boto3.client('lambda', region='us-east-1')
@@ -12,5 +13,26 @@ def iam_clent():
 
 def create_access_policy_for_lambda():
     s3_access_policy_document = {
-
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": [
+                    "s3:*",
+                    "logs:CreateLogGroup",
+                    "logs:CreateLogStream",
+                    "logs:PutLogEvents"
+                ],
+                "Effect": "Allow",
+                "Resource": "*"
+            }
+        ]
     }
+
+    return iam_clent().create_policy(
+        PolicyName='LambdaS3AccessPolicy',
+        PolicyDocument=json.dumps(s3_access_policy_document),
+        Description='Allows lambda function to access S3 resources'
+    )
+
+if __name__ == '__main__':
+    print(create_access_policy_for_lambda())
